8a959191bb4d713f078a7e284fe1efa1f89755ac
Nathan Glover <nathan@glovers.id.au>
2016-02-08 16:25:12 +0800

    Imported Themes and Get-TimeZone script

---
+++ scripts/Get-TimeZone.ps1
@@ -0,0 +1,6 @@
+﻿function Get-TimeZone($Name)
+{
+    system.timezoneinfo]::GetSystemTimeZones() | 
+    Where-Object { $_.ID -like "*$Name*" -or $_.DisplayName -like "*$Name*" } | 
+    Select-Object -ExpandProperty ID
+}
