72b94537b039cbbdff6b2282a049944f1336be0e
Nathan Glover <nathan@glovers.id.au>
2016-04-28 11:48:48 +0800

    Updated/Testing PS information fields

--- Scripts/Get-TimeZone.ps1
+++ Scripts/Get-TimeZone.ps1
@@ -1,5 +1,11 @@
 ﻿function Get-TimeZone($Name)
 {
+<#
+.Synopsis
+Retrieves Timezone data
+.DESCRIPTION
+Retrieves Timezone information for a given locale
+#>
     system.timezoneinfo]::GetSystemTimeZones() | 
     Where-Object { $_.ID -like "*$Name*" -or $_.DisplayName -like "*$Name*" } | 
     Select-Object -ExpandProperty ID
